47f228caf4d5f52aa46e27efe8e74e33cbe51be5
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-06-23 23:57:41 +0530

    User authentication done

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 const connectToDb = require('./db/db');
 
@@ -15,6 +16,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
